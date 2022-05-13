c27319b8bb60dd8734305e7c16422e540a71dbbe
Sontos Sharma <sontossharma926@gmail.com>
2022-05-13 16:09:34 +0600

    website

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+  // must be an array, could have only one element
+
+let visibilityIds = ['#counters_1','#counters_2','#counters_3'];
+// default counter class
+let counterClass ='.counter';
+// default animation speed
+let defaultSpeed = 3000;
